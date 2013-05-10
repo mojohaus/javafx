4546eba54ffd0566f930e1d5f50defee93a5eb11
Milos Kleint <mkleint@gmail.com>
2013-05-10 08:11:25 +0000

    simple fx archetype, no dependencies
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/mojo-archetypes/javafx@18283 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/resources/archetype-resources/src/main/java/MainApp.java
+++ src/main/resources/archetype-resources/src/main/java/MainApp.java
@@ -1,6 +1,7 @@
 package ${package};
 
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -9,26 +10,28 @@ import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
-
-
-    public static void main(String[] args) throws Exception {
-        launch(args);
-
-    }
-
+    @Override
     public void start(Stage stage) throws Exception {
-
-
-        String fxmlFile = "/fxml/hello.fxml";
-        FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
-
-
-        Scene scene = new Scene(rootNode, 400, 200);
-        scene.getStylesheets().add("/styles/styles.css");
-
-        stage.setTitle("Hello JavaFX and Maven");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
         stage.show();
     }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
